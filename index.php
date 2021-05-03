b3f781c4e737aea8a1731381612adb4fed10dfe5
Andrea <b.a.benvenuti3@gmail.com>
2021-05-03 16:40:27 +0200

    aggiunto testo censurato

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <?php
-
-
+$cit = "Come diceva Zarathustra: nella vita, che tu cammini e ti muovi, o siedi e aspetti, prima o poi uno stronzo lo incontri.";
+$badword = $_GET["badword"];
+// ?badword=stronzo
 ?>
 
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600&family=Zen+Dots&display=swap" rel="stylesheet">
+    <!-- /Google fonts -->
+    <link rel="stylesheet" href="css/style.css">
     <title>BadWords</title>
 </head>
+
 <body>
-    
+    <h1>Censored</h1>
+    <div class="text">
+        <h2>Testo Originale:</h2>
+        <p class="text__original"><?php echo $cit ?></p>
+    </div>
+    <?php if ($badword === "stronzo") : ?>
+        <div class="text">
+            <h2>Testo Censurato:</h2>
+            <p class="text__censored"><?php echo str_replace($badword, "***", $cit); ?></p>
+        </div>
+    <?php endif; ?>
+    <div class="text">
+        <h2><?php echo 'Numero di caratteri: '  .strlen($cit)?></h2>
+    </div>
 </body>
+
 </html>
